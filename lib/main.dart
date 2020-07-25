bb14601ec3d41d519481202e6aa4c5e8d31e672f
ranger163 <ahmed.nassar163@gmail.com>
2020-07-25 21:50:15 +0200

    Adding Floating Action Button "FAB"

--- lib/main.dart
+++ lib/main.dart
@@ -46,6 +46,11 @@ class MyHomePage extends StatelessWidget {
           ),
         ),
       ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {},
+      ),
     );
   }
 }
