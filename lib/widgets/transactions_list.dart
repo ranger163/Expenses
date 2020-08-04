44619f558f8152ba76adbb6f1e74588eec1a7d9b
ranger163 <ahmed.nassar163@gmail.com>
2020-08-04 14:05:47 +0200

    Using device size in conditions

--- lib/widgets/transactions_list.dart
+++ lib/widgets/transactions_list.dart
@@ -30,11 +30,21 @@ class TransactionsItemList extends StatelessWidget {
             ),
             subtitle:
                 Text(DateFormat.yMMMd().format(_transactionsList[index].date)),
-            trailing: IconButton(
-              icon: Icon(Icons.delete),
-              color: Theme.of(context).errorColor,
-              onPressed: () => _removeTransaction(_transactionsList[index].id),
-            ),
+            trailing: MediaQuery.of(context).size.width > 460
+                ? FlatButton.icon(
+                    icon: Icon(
+                      Icons.delete
+                    ),textColor: Theme.of(context).errorColor,
+                    label: Text('Delete'),
+                    onPressed: () =>
+                        _removeTransaction(_transactionsList[index].id),
+                  )
+                : IconButton(
+                    icon: Icon(Icons.delete),
+                    color: Theme.of(context).errorColor,
+                    onPressed: () =>
+                        _removeTransaction(_transactionsList[index].id),
+                  ),
           ),
         );
       },
